ad934d1ddd5e2df7906f0593912053489c91f32a
ChiomaE <chioma.eriken@gmail.com>
2026-02-09 09:26:29 -0600

    solved 2.1

--- index.js
+++ index.js
@@ -69,4 +69,20 @@ function printNums(num) {
   }
 }
 
-printNums(maxNum);
+// printNums(maxNum);
+
+//Part 2
+
+let data = [
+  { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+  { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+  { id: "7", name: "Bilbo", occupation: "None", age: "111" },
+];
+
+// 1
+const sortedArray = data.sort(
+  (personOne, personTwo) => personOne.age - personTwo.age,
+);
+console.log(sortedArray);
