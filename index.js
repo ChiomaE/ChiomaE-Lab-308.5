7749fcd357f086815fb14b027bc48bf353765992
ChiomaE <chioma.eriken@gmail.com>
2026-02-09 09:03:53 -0600

    finish part 3

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 // Part 1
-1.
-const numArray = [1,2,3,4,5,6]
+1;
+const numArray = [1, 2, 3, 4, 5, 6];
 
 function sum(numToSum) {
-    let sum =0;
-    for(num of numToSum){
+  let sum = 0;
+  for (num of numToSum) {
         sum += num;
     }
     return sum;
 }
-let sumofNums = sum(numArray)
+let sumofNums = sum(numArray);
 // console.log(sumofNums)
 
 //2
 
 function average(numToSum) {
-    let sum =0;
-    for(num of numToSum){
+  let sum = 0;
+  for (num of numToSum) {
         sum += num;
     }
-        let average = sum/numToSum.length;
+  let average = sum / numToSum.length;
     return average;
 }
-let averageofNums = average(numArray)
+let averageofNums = average(numArray);
 // console.log(averageofNums)
 
 //3
 
-const stringArray = ['Cat', 'Dogs', 'Numbers', 'Shoelaces'];
+const stringArray = ["Cat", "Dogs", "Numbers", "Shoelaces"];
 
-function findLongestWord(arr){
-    let longest = arr.reduce(
-        function(wordOne, wordTwo){
-            wordOne-wordTwo;
-        }
-    )
+function findLongestWord(arr) {
+  let longest = arr.reduce(function (wordOne, wordTwo) {
+    return wordOne.length > wordTwo.length ? wordOne : wordTwo;
+  });
     return longest;
 }
 
-let longestWord = findLongestWord(stringArray)
-console.log(longestWord)
+let longestWord = findLongestWord(stringArray);
+// console.log(longestWord);
+
